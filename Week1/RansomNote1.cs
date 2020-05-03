ad9387ba84523acb0ca62448483e5f7654ecdc77
Author Name <harshalbhandari48@gmail.com>
2020-05-03 19:45:51 +0530

    add a faster solution for ransom note

---
+++ Week1/RansomNote1.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Leetcode_May_Challenge.Week1
+{
+    public class RansomNote1
+    {
+        public bool CanConstruct(string ransomNote, string magazine)
+        {
+            int[] charCount = new int[26];
+
+            foreach (char c in magazine)
+            {
+                charCount[c - 'a']++;
+            }
+
+            foreach (char c in ransomNote)
+            {
+                if (charCount[c - 'a']-- == 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
