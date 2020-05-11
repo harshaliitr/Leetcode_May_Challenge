46b3b61429908d184a74ec1bc5f735f9da9055e8
harshaliitr <harshalbhandari48@gmail.com>
2020-05-12 03:12:46 +0530

    refactor flood fill code

--- Week2/FloodFillProblem.cs
+++ Week2/FloodFillProblem.cs
@@ -13,27 +13,24 @@ namespace Leetcode_May_Challenge.Week2
             var currentColor = image[sr][sc];
             if (currentColor == newColor)
                 return image;
-            image = FloodFillRecursive(image, sr, sc, newColor, currentColor);
-            return image;
-        }
-        private int[][] FloodFillRecursive(int[][] image, int sr, int sc, int newColor, int startColor)
-        {
             var rows = image.Length;
             var columns = image[0].Length;
+            return FloodFillRecursive(sr, sc);
 
-            if (sr >= rows || sr < 0 || sc >= columns || sc < 0)
+            int[][] FloodFillRecursive(int sr, int sc)
+            {
+                if (sr >= rows || sr < 0 || sc >= columns || sc < 0 || image[sr][sc] != currentColor)
+                    return image;
+
+                if (image[sr][sc] == currentColor)
+                    image[sr][sc] = newColor;
+
+                FloodFillRecursive(sr - 1, sc); // left
+                FloodFillRecursive(sr + 1, sc); // right
+                FloodFillRecursive(sr, sc - 1); // up
+                FloodFillRecursive(sr, sc + 1); // down
                 return image;
-
-            if (image[sr][sc] == startColor)
-                image[sr][sc] = newColor;
-            else
-                return image;
-
-            FloodFillRecursive(image, sr - 1, sc, newColor, startColor); // left
-            FloodFillRecursive(image, sr + 1, sc, newColor, startColor); // right
-            FloodFillRecursive(image, sr, sc - 1, newColor, startColor); // up
-            FloodFillRecursive(image, sr, sc + 1, newColor, startColor); // down
-            return image;
+            }
         }
     }
 }
