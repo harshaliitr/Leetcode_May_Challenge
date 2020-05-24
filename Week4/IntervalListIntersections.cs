eb4710dd88e7da1e627bc3960e8a5c036f4199fe
harshaliitr <harshalbhandari48@gmail.com>
2020-05-24 12:57:32 +0530

    add solution for interval intersection

--- Week4/IntervalListIntersections.cs
+++ Week4/IntervalListIntersections.cs
@@ -1,9 +1,34 @@
+using System.Collections.Immutable;
+using System.Xml.Linq;
+using System.Linq;
+using System.Collections.Generic;
 using System;
 
 namespace Leetcode_May_Challenge.Week4
 {
     public class IntervalListIntersections
     {
-        
+        public int[][] IntervalIntersection(int[][] A, int[][] B)
+        {
+            var result = new List<int[]>();
+            foreach (var pairOfA in A)
+            {
+                foreach (var pairOfB in B)
+                {
+                    if (pairOfB[0] <= pairOfA[1] && pairOfA[0] <= pairOfB[1])
+                    {
+                        int[] intersection = new int[2];
+                        intersection[0] = pairOfA[0] > pairOfB[0] ? pairOfA[0] : pairOfB[0];
+                        intersection[1] = pairOfA[1] < pairOfB[1] ? pairOfA[1] : pairOfB[1];
+                        result.Add(intersection);
+                    }
+                    else
+                    {
+                        
+                    }
+                }
+            }
+            return result.ToArray();
+        }
     }
 }
